47831c09b94639617ca9209266f74077b53c58c8
shmeadyy <shmeadyy@github.com>
2021-04-12 14:13:24 -0700

    updated token after reading the docs

--- .github/actions/readme-check/main.js
+++ .github/actions/readme-check/main.js
@@ -3,10 +3,10 @@ const github = require("@actions/github");
 
 async function run() {
     try {
-        const repoToken = core.getInput('repoToken');
-        const octokit = github.getOctokit(repoToken);
-        console.log(`This is octokit variable: ${octokit}`);
-        console.log(`This is your repoToken: ${repoToken}`);
+        const token = core.getInput('repo-token');
+        const octokit = github.getOctokit(token);
+        console.log("We're successfully running this action!")
+
     } catch (err) {
         core.setFailed(`Action failed with error: ${err}`);
     }
